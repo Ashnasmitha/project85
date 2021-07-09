8517f12dbad58b005a060ada725550eb22330fea
Ashnasmitha <75996347+Ashnasmitha@users.noreply.github.com>
2021-07-09 21:12:56 +0530

    Add files via upload

---
+++ screens/Profile.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default class Profile extends React.Component{
+render(){
+  return(
+    <View style={{justifyContent:'center',alignItems:'center',flex:1}}>
+      <Text>Profile</Text>
+    </View>
+  )
+}
+}
